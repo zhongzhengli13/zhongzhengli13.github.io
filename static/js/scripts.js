40ab078770b0b4a0058446d2687c8e59d5a0c4d5
zhongzhengli13 <878954714@qq.com>
2026-06-20 19:43:10 +0800

    fixed some bugs 007

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -197,5 +197,15 @@ window.addEventListener('DOMContentLoaded', event => {
         loadContent();
     });
 
+    // Navbar scroll background
+    const header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+
     loadContent();
 });
